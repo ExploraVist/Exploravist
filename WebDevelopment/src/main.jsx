07bbc49c07cddce296ad41aae29ca3ec8f3da65f
bilal-abraham <bilal.abraham.717@gmail.com>
2025-03-05 16:20:01 -0800

    Updated home page

--- WebDevelopment/src/main.jsx
+++ WebDevelopment/src/main.jsx
@@ -4,13 +4,16 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from './routes/Home.jsx'
 import Updates from './routes/Updates.jsx'
 import Contact from './routes/Contact.jsx'
-import './index.css'
+import Community from './routes/Community.jsx'
 import Error404 from './routes/Error404.jsx'
+import './index.css'
+
 
 const router = createBrowserRouter([
   {path: '/', element: <Home/>},
   {path: '/updates', element: <Updates/>},
   {path: '/contact', element: <Contact/>},
+  {path: '/community', element: <Community/>},
   {path: '*', element: <Error404/>}
 ])
 
--- WebDevelopment/src/routes/Updates.jsx
+++ WebDevelopment/src/routes/Updates.jsx
@@ -23,6 +23,38 @@ const Updates = () => {
           <section className='updates_generations'>
             <h1 className='updates_header'>Progress Updates</h1>
             <div className='updates_gen'>
+              <GenerationTitle gen_num='4' gen_date='04.21.24' gen_title='Our ready for market prototype!'/>
+              <span className='updates_details_toggle' onClick={() => setGenThreeDetails(!genThreeDetails)}>
+                <ChevronDown className={`icon ${genThreeDetails ? 'rotate-down' : 'rotate-right'}`} size={30}/>
+                <strong>Details</strong>
+              </span>
+              {genThreeDetails && 
+                <div className='updates_details'>
+                  <p><b>This version responds in 6 seconds with 95%+ success rate in descriptions, and thanks to our model switch to Claude Haiku, is 40x cheaper!</b></p>
+                  <p><b>We also now have a UI that allows you to:</b></p>
+                  <ul>
+                    <li>Adjust Volume</li>
+                    <li>Check the battery level</li>
+                    <li>Change models from GPT-4 to Claude Haiku</li>
+                    <li>Change response modes from short description (1-2 sentences) to long description (1-2 paragraphs)!</li>
+                  </ul>
+                  <p>After positive feedback from visiting the San Bernardino lighthouse for the blind, we think this version is ready for the market! 
+                    Because of this we are just doing one more iteration to Gen 4 which will have a PCB that allows us to mass produce it. On top of that we hope to also add a few more easy to implement features!</p>
+                  <p><b>We want to add:</b></p>
+                  <ul>
+                    <li><b>Voice Input: </b>Ask questions about pictures for a specific description</li>
+                    <li><b>Voice Assistant: </b>Ask any question to the AI without a picture</li>
+                    <li><b>Voice Memos: </b>Record notes for yourself so you can replay them</li>
+                    <li><b>App Support: </b>Partner IPhone/Android app to connect your device to the internet without needing to call with us to set it up!</li>
+                  </ul>
+                  <p>If you are interested in testing the device for free, visit our<Link className='updates_details_link' to='/contact'>Contact Page</Link>.</p>
+                  <h3>Click the picture to see a video of it working!</h3>
+                  <ImageLink image={imgGen3} link='https://drive.google.com/file/d/1CzRA4ILdGap4miWN8zYAqFEqi0FCe1_o/view'/>
+                  <div className='updates_details_end'/>
+                </div>
+              }
+            </div>
+            <div className='updates_gen'>
               <GenerationTitle gen_num='3' gen_date='04.21.24' gen_title='Our ready for market prototype!'/>
               <span className='updates_details_toggle' onClick={() => setGenThreeDetails(!genThreeDetails)}>
                 <ChevronDown className={`icon ${genThreeDetails ? 'rotate-down' : 'rotate-right'}`} size={30}/>
